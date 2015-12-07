6a5cecbcc1733b8df12b9fe431108ce76aea1490
jlenhard <jjlenhard@gmail.com>
2015-12-06 23:59:18 -0500

    fix directions

--- getSample.m
+++ getSample.m
@@ -9,13 +9,13 @@ pxvals = zeros(1,slen);
 for i=1:slen
     tvals(1,i) = i;
     %N,E,S,W = 0,1,2,3 
-    if(sdir == 2)
+    if(sdir == 3)
         pxvals(1,i) = img(sr,scol-(i-1));
-    elseif(sdir == 1)
-        pxvals(1,i) = img(sr-(i-1),scol);    
     elseif(sdir == 0)
+        pxvals(1,i) = img(sr-(i-1),scol);    
+    elseif(sdir == 1)
         pxvals(1,i) = img(sr,scol+(i-1));
-    elseif(sdir == 3 )
+    elseif(sdir == 2 )
         pxvals(1,i) = img(sr+(i-1),scol);    
     end        
 end
